5d87d6757b96e644a1dd7217acb19482ff8b6cc1
abizmo <abizmo@gmail.com>
2018-05-31 15:20:21 +0100

    Corregido del todo lo del problema de redireccion

--- src/App.js
+++ src/App.js
@@ -4,34 +4,30 @@ import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import Layout from './components/Layout';
 import { BurgerBuilder, Checkout, Orders, Auth, Logout } from './containers';
 import { tryAutoAuthentication } from './store/actions/auth';
-import { Aux } from './utils';
 
 class App extends Component {
   componentDidMount () {
     this.props.onTryAutoAuthentication();
   };
-  
+
   render() {
     return (
       <div>
         <Layout>
-          <Switch>
-            {
-              this.props.isLogged ?
-              <Aux>
-                <Route path="/checkout" component={Checkout}/>
-                <Route path="/orders" component={Orders}/>
-                <Route path="/logout" component={Logout}/>
-                <Route path="/" exact component={BurgerBuilder}/>
-                <Route render={ () => <Redirect to="/" /> } />
-              </Aux> :
-              <Aux>
-                <Route path="/auth" component={Auth}/>
-                <Route path="/" exact component={BurgerBuilder}/>
-                <Route render={ () => <Redirect to="/" /> } />
-              </Aux>
-            }
-          </Switch>
+          {
+            this.props.isLogged ?
+            <Switch>
+              <Route path="/checkout" component={Checkout}/>
+              <Route path="/orders" component={Orders}/>
+              <Route path="/logout" component={Logout}/>
+              <Route path="/" exact component={BurgerBuilder}/>
+              <Route render={ () => <Redirect to="/" /> } />
+            </Switch> :
+            <Switch>
+              <Route path="/auth" component={Auth}/>
+              <Route path="/" exact component={BurgerBuilder}/>
+            </Switch>
+          }
         </Layout>
       </div>
     );
